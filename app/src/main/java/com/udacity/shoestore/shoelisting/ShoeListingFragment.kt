3db02a239bdd11220c98d0597cdf57b9a8394150
isosic <ivan.sosic@asseco-see.hr>
2021-02-11 13:37:14 +0100

    code cleanup

--- app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListingFragment.kt
@@ -1,18 +1,12 @@
 package com.udacity.shoestore.shoelisting
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.activitymain.MainActivityViewModel
@@ -25,7 +19,7 @@ class ShoeListingFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeListingBinding
     private lateinit var viewModel: ShoeListingViewModel
-    private lateinit var mainViewModel: MainActivityViewModel //by activityViewModels()
+    private lateinit var mainViewModel: MainActivityViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,14 +32,8 @@ class ShoeListingFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(ShoeListingViewModel::class.java)
         mainViewModel = ViewModelProvider(requireActivity())[MainActivityViewModel::class.java]
 
-
         setObservers()
-
-
-//TODO use databinding
-        binding.fragmentShoeListingFab.setOnClickListener {
-            findNavController().navigate(R.id.action_shoeListingFragment_to_shoeDetailsFragment)
-        }
+        setListeners()
 
         return binding.root
     }
@@ -56,6 +44,12 @@ class ShoeListingFragment : Fragment() {
                 addLayout(shoe)
             }
         })
+    }
+
+    private fun setListeners() {
+        binding.fragmentShoeListingFab.setOnClickListener {
+            findNavController().navigate(R.id.action_shoeListingFragment_to_shoeDetailsFragment)
+        }
     }
 
     private fun addLayout(shoe: Shoe) {
--- app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListingViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListingViewModel.kt
@@ -7,13 +7,13 @@ import com.udacity.shoestore.models.Shoe
 
 class ShoeListingViewModel : ViewModel() {
 
-    val shoeList: LiveData<Shoe>
-        get() = _shoeList
-
-    private val _shoeList = MutableLiveData<Shoe>()
-
-    private val eventShoeAdded: LiveData<Shoe>
-        get() = _eventShoeAdded
-
-    private val _eventShoeAdded = MutableLiveData<Shoe>()
+//    val shoeList: LiveData<Shoe>
+//        get() = _shoeList
+//
+//    private val _shoeList = MutableLiveData<Shoe>()
+//
+//    private val eventShoeAdded: LiveData<Shoe>
+//        get() = _eventShoeAdded
+//
+//    private val _eventShoeAdded = MutableLiveData<Shoe>()
 }
